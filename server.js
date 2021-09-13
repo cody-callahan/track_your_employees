4a437bd6601176d34069f9e12628ef05959ed8fa
Cody Callahan <codycallahan02@gmail.com>
2021-09-12 20:51:54 -0700

    create options for users

--- server.js
+++ server.js
@@ -3,5 +3,13 @@ const util = require("util");
 const db = require("./db/connection");
 
 
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('We are connected to the DB!!!');
+    promptUser();
+  });
+
+const userOptions = ['See all employees', 'See all roles', 'See all departments', 'Add dept', 'Add employee', 'Add new role', 'Update employee role']
 
 
