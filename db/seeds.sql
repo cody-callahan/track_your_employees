0d07d2cc8612c8cee178b25df8aa241060cbc776
Cody Callahan <codycallahan02@gmail.com>
2021-09-12 21:27:31 -0700

    add ability to see data via inquirer prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO department(department_name)
-VALUES ("Administration"), ("Pro Scouting"), ('Ama Scouting'), ("Advance Scouting"), ("International Scouting"), ("Player Development");
+VALUES ("Administration"), ("Pro Scouting"), ('Ama Scouting'), ("Advance Scouting"), ("International Scouting"), ("Player Development"), ("Research and Development");
 
 INSERT INTO roles(title, salary, department_id)
-VALUES ("Analyst", 45000, 1), ("Coach", 50000, 2), ("Coordinator", 70000, 3), ("Manager", 80000, 4), ("Assistant Director", 100000, 5), ("Director", 150000, 6), ("Assistant GM", 350000, 7), ("GM", 350000, 8), ("President", 350000, 9);
+VALUES ("Analyst", 45000, 7), ("Coach", 50000, 4), ("Coordinator", 70000, 1), ("Pro Scout", 80000, 2), ("Assistant Director", 100000, 5), ("Director", 150000, 3), ("Assistant GM", 350000, 6), ("GM", 350000, 1), ("President", 350000, 1);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES('Cody', 'Callahan', 1, 1), ('Max', 'Deric', 2, 2), ('Jean', 'Roberts', 3, 3), ('Michelle', 'Ann', 3, 3), ('Mike', 'Smith', 1, 1);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const inquirer = require("inquirer");
-const util = require("util");
 const db = require("./db/connection");
+const inquirer = require("inquirer");
 
+const util = require("util");
 
 // Start server after DB connection
 db.connect(err => {
@@ -10,6 +10,55 @@ db.connect(err => {
     promptUser();
   });
 
-const userOptions = ['See all employees', 'See all roles', 'See all departments', 'Add dept', 'Add employee', 'Add new role', 'Update employee role']
+const userOptions = ['See all employees', 'See all roles', 'See all departments', 
+'Add employee', 'Add dept', 'Add new role', 'Update employee role']
 
 
+const promptUser = () => {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'Please select an option below:',
+            choices: userOptions,
+            default: 0
+        },
+    ])
+        .then(selectionData => {
+            if (selectionData.action === 'See all employees') {
+                db.query(`SELECT * FROM employee`, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.table(row);
+                    }
+                    promptUser();
+
+                });
+            } if (selectionData.action === 'See all roles') {
+                db.query(`SELECT * FROM roles`, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.table(row);
+                    }
+                    promptUser();
+
+                });
+            } if (selectionData.action === 'See all departments') {
+                db.query(`SELECT * FROM department`, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.table(row);
+                    }
+                    promptUser();
+
+                });
+            } 
+        })        
+}
